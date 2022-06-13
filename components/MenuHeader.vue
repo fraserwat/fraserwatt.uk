1a79606f37018aec0554e1f010356bfca8e6b015
Fraser Watt <fsrwatt@gmail.com>
2022-06-13 21:42:23 +0100

    First commit

---
+++ components/MenuHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="[ box-flex space-between ]">
+        <h2 class="[ fraser ] [ mobile-on color-white text-600 ]">Fraser Watt</h2>
+        <h2 class="[ fraser ] [ mobile-off color-white text-600 ]">FW</h2>
+        <nav class="[ menu ] [ mobile-on ]">
+            <ul class="box-flex">
+                <li><a href="https://www.linkedin.com/in/fraser-watt/">Data</a></li>
+                <!-- <li><a href="">Web</a></li> -->
+                <li><a href="">Blog</a></li>
+                <li><button class="button">Contact</button></li>
+            </ul>
+        </nav>
+        <button class="[ hamburger ] [ mobile-off ]"></button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "MenuHeader"
+}
+</script>
+
+<style scoped>
+    .box-flex {
+        align-items: center;
+    }
+    li + li {
+        margin-left: 3rem;
+    }
+    a {
+        color: white;
+        font-weight: bold;
+        text-transform: uppercase;
+        text-decoration: none;
+    }
+    .button {
+        border-radius: 8px;
+        padding: .5rem 1rem;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="[ container ] [ max-width-1100 ]">
+    <menu-header />
+    <main class="pad-bottom-900" >
+      <section class="[ greeting ] [ ]">
+        <h1 class="color-white">
+          Hi, I'm Fraser! I build websites, use data to grow organisations, and write about technology.
+        </h1>
+        <div class="profile-pic-container ] [  ]">
+          <img class="[ profile-pic ] [ rad-max ]" src="/images/fraser-watt.png" alt="Fraser Watt">
+        </div>
+      </section>
+      <section class="[ biog-contact ] [ ]">
+        <article class="[ maintext ] [ bg-white rad-10 flow shadow ]">
+          <p>I’ve worked in Data & Analyics for eight years, and as a self-employed contractor since the first half of 2021.</p>
+          <p>Using a data-driven approach to enable growth across end-to-end analytics pipelines, I work where the project requires — from ETL and analytics engineering, to visualisation and commercial insights. I’ve had success doing this in both a B2B and B2C context at companies as varied as <b>Paddle</b>, <b>Facebook</b>, and <b>Otrium</b>.</p>
+          <p>I also write about the intersection of technology and politics, and have been featured in <b>Tribune</b> and <b>Jacobin</b> magazines.</p>
+          <p>From time to time I'm also available for front-end web development, where my focus is on accessible, lightweight and browser-agnostic implementation of your design.</p>
+        </article>
+        <div class="[ biog-contact__sub-box ] [ box-flex ]">
+          <aside class="[ ] [ bg-blue color-white rad-10 shadow ]">
+            <p>I am taking Data & Analytics contracts for <b>Q3 2022</b>, and web development work on an adhoc basis.</p>
+          </aside>
+          <form action="" class="[ contact-form ] [ bg-white color-navy flow rad-10 box-flex shadow ]">
+            <input id="form-name" type="text" name="name" placeholder="Your Name">
+            <input id="form-email" type="email" name="email" placeholder="Email Address">
+            <textarea id="form-message" name="message" placeholder="Your message here"></textarea>
+            <p>Please include details on your project, what services you require, and any rough timeframes or budgetary information.</p>
+            <button type="submit" class="[ button ] [ bg-blue color-white ]">Contact me</button>
+          </form>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import MenuHeader from '~/components/MenuHeader.vue'
+export default {
+  name: 'IndexPage',
+  components: { MenuHeader },
+}
+</script>
